63996200be9889828cd638545ea9c19c6f067cf2
Kresna Rangga Darmansyah <kresnard@gmail.com>
2022-08-18 17:41:30 +0700

    Adding scroll smooth internl route homepage product

---
+++ src/config/firebase/index.js
@@ -0,0 +1,47 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+    getAuth,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword,
+    signOut
+} from 'firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAm4R9GoqWYs0O6joZQOuutNkPHjMpW4nk",
+    authDomain: "e-commerce-shop-9b3c3.firebaseapp.com",
+    projectId: "e-commerce-shop-9b3c3",
+    storageBucket: "e-commerce-shop-9b3c3.appspot.com",
+    messagingSenderId: "331639752447",
+    appId: "1:331639752447:web:63c9bedc2530899b1f6eca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const logInWithEmailAndPassword = async (email, password) => {
+    try {
+    await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+const signUpWithEmailAndPassword = async (name, email, password) => {
+    try {
+    await createUserWithEmailAndPassword(auth, name, email, password);
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+
+
+export {auth, logInWithEmailAndPassword, signUpWithEmailAndPassword};
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,21 @@
-import React from "react"
+import React, {useState} from "react";
+import { auth, logInWithEmailAndPassword } from "config/firebase";
 
 
 const Login = () => {
+
+    const [login, setLogin] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleLogin = async (e, param) => {
+        e.preventDefault()
+        if (param === "login") {
+            await logInWithEmailAndPassword(login.email, login.password);
+        }
+    }
+
     return (
         <div className="container">
             
@@ -9,10 +23,32 @@ const Login = () => {
                 <h1>Sign In</h1>
                 <form action="" method="">
                     <p>Email</p>
-                    <input type="text" name="email" placeholder="Input your email"/><br/>
+                    <input
+                    type="text"
+                    name="email"
+                    placeholder="Input your email"
+                    value={login.email}
+                    onChange={ ((e) => 
+                        setLogin({
+                            ...login,
+                            email: e.target.value,
+                        })
+                    )}
+                    /><br/>
                     <p>Password</p>
-                    <input type="password" name="pass" placeholder="Input your password"/><br/>
-                    <button>Sign In</button>
+                    <input
+                    type="password"
+                    name="pass"
+                    placeholder="Input your password"
+                    value={login.password}
+                    onChange={ ((e) => 
+                        setLogin({
+                            ...login,
+                            password: e.target.value,
+                        })
+                    )}
+                    /><br/>
+                    <button onClick={(e) => handleLogin(e,"login")}>Sign In</button>
                 </form>
                 
             </div>
