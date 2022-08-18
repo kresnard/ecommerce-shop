17fe534996ee8110b422bbd55cf1d5f0e6279b07
Kresna Rangga Darmansyah <kresnard@gmail.com>
2022-08-18 13:55:12 +0700

    Adding scroll smooth internl route homepage product

--- src/component/header.js
+++ src/component/header.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link as Scroll } from "react-scroll";
 import { Link } from "react-router-dom";
 import { images } from "assets";
 
@@ -16,6 +17,9 @@ const Header = () => {
                 <nav>
                     <ul>
                         <li className="home"><Link to ="/">Home</Link></li>
+                        <li>
+                            <Scroll to="products" spy={true} smooth={true} offset={250} duration={500}>Product</Scroll>
+                        </li>
                         <li><Link to="/Cart">Cart</Link></li>
                         <li><Link to="/sign-in">Sign In</Link></li>
                         <li><Link to="/sign-up">Sign Up</Link></li>
