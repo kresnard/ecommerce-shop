2a3e65789dedfc158fecdbe1035eb9bfbbc67224
Kresna Rangga Darmansyah <kresnard@gmail.com>
2022-08-26 12:53:36 +0700

    added logic routing when access cart without sign in directly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import './App.css';
+import "./App.css";
 import Header from "./component/header";
-import HomePage from './pages/homepage';
-import DetailProduct from 'pages/detail-products/index.js';
-import Cart from 'pages/cart';
-import Login from './pages/login';
-import Register from './pages/register';
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import HomePage from "./pages/homepage";
+import DetailProduct from "pages/detail-products/index.js";
+import Cart from "pages/cart";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { auth } from "config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <BrowserRouter>
       <Header />
-        <Routes>
-          <Route exact path="/" element={<HomePage />} />
-          <Route exact path="/DetailProduct/:id" element={<DetailProduct />} />
-          <Route exact path="/Cart" element={<Cart />} />
-          <Route exact path="/sign-in" element={<Login />} />
-          <Route exact path="/sign-up" element={<Register />} />
-        </Routes>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/DetailProduct/:id" element={<DetailProduct />} />
+        <Route exact path="/Cart" element={user ? <Cart /> : <Login />} />
+        <Route exact path="/sign-in" element={<Login />} />
+        <Route exact path="/sign-up" element={<Register />} />
+      </Routes>
     </BrowserRouter>
   );
 }
